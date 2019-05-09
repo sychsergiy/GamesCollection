7f90edacdc77133276d758c732cd7ab355a7c606
sheva <shevchuksanya983@gmail.com>
2019-05-09 23:53:28 +0300

    battlefield view, game mode

---
+++ battleship/player_battlefield.py
@@ -0,0 +1,79 @@
+import typing as t
+
+from battleship.battlefield_view import BattlefieldView
+from battleship.game_mode import GameMode
+from battleship.shot_manager import ShotManager, ShotResultEnum
+from battleship.ships_locator import ShipsLocator
+from battleship.ship_locator import ShipLocator
+from battleship.ship_location import (
+    VerticalShipLocation,
+    HorizontalShipLocation,
+)
+from battleship.ship import Ship
+from battleship.cell import Cell
+
+from enum import Enum
+
+
+class ShipRotationEnum(Enum):
+    VERTICAL = 0
+    HORIZONTAL = 1
+
+
+class PlayerBattlefield(object):
+    def __init__(
+            self,
+            game_mode: GameMode,
+
+    ):
+        # todo: located all ships from game_mode and than start game
+
+        # todo: move all class to arguments
+        self._ships_locator = ShipsLocator(game_mode.battlefield)
+        self._shot_manager = ShotManager(
+            game_mode.battlefield, self._ships_locator
+        )
+        self._ship_locator = ShipLocator(
+            game_mode.battlefield, self._ships_locator
+        )
+        self._view = BattlefieldView(
+            game_mode.battlefield, self._ships_locator, self._shot_manager
+        )
+
+    def locate_ship(
+            self,
+            x: int,
+            y: int,
+            ship_size: int,
+            rotation: ShipRotationEnum = ShipRotationEnum.HORIZONTAL
+    ) -> bool:
+        assert isinstance(rotation, ShipRotationEnum)
+        cell = Cell(x, y)
+        ship = Ship(ship_size)
+        if rotation == ShipRotationEnum.VERTICAL:
+            ship_location = VerticalShipLocation(ship, cell)
+        elif rotation == ShipRotationEnum.HORIZONTAL:
+            ship_location = HorizontalShipLocation(ship, cell)
+        return self._ship_locator.locate_ship(ship_location)
+
+    @property
+    def ships(self) -> t.List[Ship]:
+        ships = [
+            ship_location.ship for ship_location in
+            self._ships_locator.ships_locations
+        ]
+        return ships
+
+    def shot(self, x: int, y: int) -> ShotResultEnum:
+        cell = Cell(x, y)
+        # todo: try Except
+        return self._shot_manager.shot(cell)
+
+    def is_game_over(self) -> bool:
+        return all([ship.is_destroyed() for ship in self.ships])
+
+    def print_battlefield_view(self, show_unwounded_ships_cells):
+        # todo: remove method from current class
+        self._view.draw(show_unwounded_ships_cells)
+        for row in self._view.battlefield_matrix:
+            print(row)
