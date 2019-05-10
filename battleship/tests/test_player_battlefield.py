b99c080c77316035eb6645b3827ca7cbff6064aa
Uncle <alex.the.kovalenko@gmail.com>
2019-05-10 22:57:08 +0300

    ame ships locating step added

--- battleship/tests/test_player_battlefield.py
+++ battleship/tests/test_player_battlefield.py
@@ -1,4 +1,5 @@
 from battleship.player_battlefield import PlayerBattlefield
+from battleship.cell import Cell
 from battleship.player import Player
 from battleship.battlefield import Battlefield
 from battleship.game_mode import GameMode
@@ -9,13 +10,14 @@ def test_player_battlefield_not_ready_to_start():
     game_mode = GameMode(Battlefield(4, 4), ship_size_count_map)
 
     p_b = PlayerBattlefield(Player("player"), game_mode)
-    assert p_b.locate_ship(0, 0, ship_size=1) == True
-    assert p_b.locate_ship(0, 0, ship_size=1) == False
+    cell = Cell(0, 0)
+    assert p_b.locate_ship(cell, ship_size=1) == True
+    assert p_b.locate_ship(cell, ship_size=1) == False
     assert p_b._ships_counter._ship_size_count_map[1] == 3
 
-    assert p_b.ready_to_start == False
-    assert p_b.set_ready_to_start() == False
-    assert p_b.ready_to_start == False
+    assert p_b.ships_locating_finished == False
+    assert p_b.finish_ships_locating() == False
+    assert p_b.ships_locating_finished == False
 
 
 def test_player_battlefield_ready_to_start():
@@ -23,11 +25,11 @@ def test_player_battlefield_ready_to_start():
     game_mode = GameMode(Battlefield(4, 4), ship_size_count_map)
 
     p_b = PlayerBattlefield(Player("player"), game_mode)
-    assert p_b.locate_ship(0, 0, ship_size=1) == True
-    assert p_b.locate_ship(3, 3, ship_size=1) == True
-    assert p_b.locate_ship(0, 1, ship_size=1) == False
+    assert p_b.locate_ship(Cell(0, 0), ship_size=1) == True
+    assert p_b.locate_ship(Cell(3, 3), ship_size=1) == True
+    assert p_b.locate_ship(Cell(0, 1), ship_size=1) == False
     assert p_b._ships_counter._ship_size_count_map[1] == 0
 
-    assert p_b.ready_to_start == False
-    assert p_b.set_ready_to_start() == True
-    assert p_b.ready_to_start == True
+    assert p_b.ships_locating_finished == False
+    assert p_b.finish_ships_locating() == True
+    assert p_b.ships_locating_finished == True
