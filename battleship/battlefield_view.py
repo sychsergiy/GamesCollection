b2584071bb2d2df26a7befe32fc2ca32e94f5c3c
kershkov <kershkov@gmail.com>
2019-05-10 14:58:27 +0300

    simplifeid game class added

--- battleship/battlefield_view.py
+++ battleship/battlefield_view.py
@@ -20,7 +20,7 @@ class BattlefieldView(object):
     def _draw_empty_cells(self, battlefield: Battlefield):
         self._battlefield_matrix = [
             # empty cells
-            ['0' for _ in range(battlefield.width)]
+            [' 0 ' for _ in range(battlefield.width)]
             for _ in range(battlefield.height)
         ]
 
@@ -34,25 +34,25 @@ class BattlefieldView(object):
 
     def _draw_hited_cells(self):
         for cell in self._shot_manager.hited_cells:
-            self._set_matrix_cell(cell, '1')
+            self._set_matrix_cell(cell, ' * ')
 
     def _draw_ships(self, show_unwounded_ships_cells: bool):
         for ship_location in self._ships_locator.ships_locations:
             if ship_location.ship.is_destroyed():
                 for cell in ship_location.get_ship_cells():
                     # destroyed ships
-                    self._set_matrix_cell(cell, '2')
+                    self._set_matrix_cell(cell, '[x]')
             else:
                 ship_cells = ship_location.get_ship_cells()
                 if show_unwounded_ships_cells:
                     for cell in ship_cells:
                         # unwounded ship cells
-                        self._set_matrix_cell(cell, '3')
+                        self._set_matrix_cell(cell, '[ ]')
 
                 for index in ship_location.ship.get_destroyed_cells_indexes():
                     ship_cell = ship_cells[index]
                     # wounded but not destroyed ship cells
-                    self._set_matrix_cell(ship_cell, '4')
+                    self._set_matrix_cell(ship_cell, '[*]')
 
     def draw(self, show_unwounded_ships_cells: bool):
         # self._draw_empty_cells() // already called in __init__ todo: refactor
--- battleship/game.py
+++ battleship/game.py
@@ -1,3 +1,4 @@
+from battleship.gun import Gun
 from battleship.player import Player
 from battleship.game_mode import GameMode
 from battleship.player_battlefield import PlayerBattlefield
@@ -10,6 +11,45 @@ class Game(object):
             second_player: Player,
             game_mode: GameMode,
     ):
-        self._first_player_battlefield = PlayerBattlefield(first_player,
-                                                           game_mode)
-        self._second_player = PlayerBattlefield(second_player, game_mode)
+        self._first_player_battlefield = PlayerBattlefield(
+            first_player, game_mode
+        )
+        self._second_player_battlefield = PlayerBattlefield(
+            second_player, game_mode
+        )
+        self._first_player_turn = False
+
+    def next_turn(self, x: int, y: int):
+        if self._first_player_turn:
+            self._first_player_turn = not self._first_player_turn
+            return self._player_turn(self._first_player_battlefield, x, y)
+        else:
+            self._first_player_turn = not self._first_player_turn
+            return self._player_turn(self._second_player_battlefield, x, y)
+
+    def print_battlefields(self):
+        # todo: get matrix and return
+        if self._first_player_turn:
+            self._first_player_battlefield.print_battlefield_view(True)
+            print('')
+            self._second_player_battlefield.print_battlefield_view(False)
+        else:
+            self._second_player_battlefield.print_battlefield_view(True)
+            print('')
+            self._first_player_battlefield.print_battlefield_view(False)
+
+    def _player_turn(
+            self,
+            player_battlefield: PlayerBattlefield,
+            x: int,
+            y: int,
+    ):
+        shot_result = player_battlefield.shot(x, y)
+        if shot_result == Gun.ShotResultEnum.SHIP_DESTROYED:
+            # todo: handle game over
+            is_game_over = player_battlefield.is_all_ships_destroyed()
+            if is_game_over:
+                raise Exception(
+                    f"Game over, {player_battlefield._player.name} winner"
+                )
+        return shot_result
