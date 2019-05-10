358dbae73311931b82ceb9f6875956f6dda890b1
sheva <shevchuksanya983@gmail.com>
2019-05-10 23:11:43 +0300

    minor changes

--- games_collection/games/battleship/tests/test_player_battlefield.py
+++ games_collection/games/battleship/tests/test_player_battlefield.py
@@ -1,6 +1,5 @@
-from games_collection.games.battleship.player_battlefield import PlayerBattlefield
+from games_collection.games.battleship.battleship_field import BattleshipField
 from games_collection.games.battleship.cell import Cell
-from games_collection.games.battleship.player import Player
 from games_collection.games.battleship.battlefield import Battlefield
 from games_collection.games.battleship.game_mode import GameMode
 
@@ -9,7 +8,7 @@ def test_player_battlefield_not_ready_to_start():
     ship_size_count_map = {1: 4, 2: 2}
     game_mode = GameMode(Battlefield(4, 4), ship_size_count_map)
 
-    p_b = PlayerBattlefield(Player("player"), game_mode)
+    p_b = BattleshipField(game_mode)
     cell = Cell(0, 0)
     assert p_b.locate_ship(cell, ship_size=1) == True
     assert p_b.locate_ship(cell, ship_size=1) == False
@@ -24,7 +23,7 @@ def test_player_battlefield_ready_to_start():
     ship_size_count_map = {1: 2}
     game_mode = GameMode(Battlefield(4, 4), ship_size_count_map)
 
-    p_b = PlayerBattlefield(Player("player"), game_mode)
+    p_b = BattleshipField(game_mode)
     assert p_b.locate_ship(Cell(0, 0), ship_size=1) == True
     assert p_b.locate_ship(Cell(3, 3), ship_size=1) == True
     assert p_b.locate_ship(Cell(0, 1), ship_size=1) == False
