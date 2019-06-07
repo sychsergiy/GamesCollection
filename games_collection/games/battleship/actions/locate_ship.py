cae61adda218b4394cbccde762d989e072a771a5
Sergiy <serhii.sych@smashdocs.net>
2019-06-07 08:24:43 +0300

    battleship locate ship action handler added

--- games_collection/games/battleship/actions/locate_ship.py
+++ games_collection/games/battleship/actions/locate_ship.py
@@ -1,3 +1,4 @@
+from games_collection.player import Player
 from games_collection.actions_handler import (
     AbstractAction,
     AbstractActionResult,
@@ -5,15 +6,40 @@ from games_collection.actions_handler import (
 )
 
 from games_collection.games.battleship.cell import Cell
+from games_collection.games.battleship.players_battleship_fields import (
+    PlayersBattleshipFields
+)
 
 
 class LocateShipAction(AbstractAction):
-    def __init__(self, cell: Cell, ship_size: int):
+    def __init__(self, player: Player, cell: Cell, ship_size: int):
+        self.player = player
         self.cell = cell
         self.ship_size = ship_size
 
 
 class LocateShipActionResult(AbstractActionResult):
-    def __init__(self, located: bool, ships_left: dict):
+    def __init__(self, located: bool, ships_left: dict, current_field: list):
         self.located = located
         self.ships_left = ships_left
+        self.current_field = current_field
+
+
+class LocateShipActionHandler(AbstractActionHandler):
+    def __init__(self, players_battleship_fields: PlayersBattleshipFields):
+        self._players_battleship_fields = players_battleship_fields
+
+    def handle(self, action: LocateShipAction) -> LocateShipActionResult:
+        battleship_field = (
+            self._players_battleship_fields.get_player_battleship_field(
+                action.player
+            )
+        )
+        located = battleship_field.locate_ship(action.cell, action.ship_size)
+        # todo: get ships_left from battleship_field.ships_counter
+        action_result = LocateShipActionResult(
+            located=located,
+            ships_left={},
+            current_field=battleship_field.get_battlefield_view(True)
+        )
+        return action_result
